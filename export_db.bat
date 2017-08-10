5dfa1c46479dd2be3f1889dcdaaeff980868200a
abrahambruck <30836926+abrahambruck@users.noreply.github.com>
2017-08-10 10:31:49 -0400

    Add files via upload

--- export_db.bat
+++ export_db.bat
@@ -1,3 +1,6 @@
+rem export_db.bat
+rem 
 cd \Users\Public
 exp abraham/bruck@orcl full=y file=\Users\Public\myexp.dmp
+del \Users\Public\oraTopTal.dmp
 expdp abraham/bruck@orcl content=ALL FULL=Y directory=dmpdir dumpfile=oraTopTal.dmp NOLOGFILE=YES
